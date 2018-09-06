6755eb2663edbd56e7bdda7db74bfd53358e4ddd
mmirzabaig <35893408+mmirzabaig@users.noreply.github.com>
2018-09-06 03:18:10 -0500

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,64 @@
+var cards = [ 
+   {
+      rank: "queen",
+      suit: "hearts",
+      cardImage: "images/queen-of-hearts.png"
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png"
+   },
+   {
+      rank: "king",
+      suit: "hearts",
+      cardImage: "images/king-of-hearts.png"
+   },
+   {
+      rank: "king",
+      suit: "diamonds",
+      cardImage: "images/king-of-diamonds.png"
+   }
+];
+
+function createBoard() {
+
+   var board = document.getElementById("game-board");
+
+   for (var i=0; i<cards.length; i++) {
+      var newCardImg = document.createElement("img");
+      newCardImg.setAttribute("src","images/back.png");
+      newCardImg.setAttribute("data-id",i);
+
+      newCardImg.addEventListener("click",flipCard);
+
+      board.appendChild(newCardImg);
+   }
+}
+
+var cardsInPlay = [];
+
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+
+      if (cardsInPlay[0] === cardsInPlay[1]) {
+         alert("Match!");
+      } else {
+         alert("Sorry, try again.");
+      }
+   }
+}
+function flipCard() {
+
+   var cardId = this.getAttribute("data-id");
+
+   cardsInPlay.push(cards[cardId].rank);
+   this.setAttribute("src",cards[cardId].cardImage);
+
+   console.log("User flipped "+cards[cardId].rank);
+
+   checkForMatch();
+	
+}
+createBoard();
+
